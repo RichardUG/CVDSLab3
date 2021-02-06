6920ac50525891dda1c647a36965ae0ffb034116
RichardUG <rsug.rsug@gmail.com>
2021-02-06 10:04:18 -0500

    Registraduria

--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,9 +1,24 @@
 package edu.eci.cvds.tdd.registry;
-
+import java.util.ArrayList;
 public class Registry {
-    public RegisterResult registerVoter(Person p) {
+    private ArrayList<Integer> registrados = new ArrayList<Integer>();
 
-        // TODO Validate person and return real result.
-        return RegisterResult.VALID;
+    public RegisterResult registerVoter(Person p){
+        if(!p.isAlive()){
+            return RegisterResult.DEAD;
+        }
+        else if(registrados.contains(p.getId())){
+            return RegisterResult.DUPLICATED;
+        }
+        else if(p.getAge()<18 && p.getAge()>0){
+            return RegisterResult.UNDERAGE;
+        }
+        else if(p.getAge()<0 || p.getAge()>120){
+            return RegisterResult.INVALID_AGE;
+        }
+        else{
+            registrados.add(p.getId());
+            return RegisterResult.VALID;
+        }
     }
 }
